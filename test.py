5eaf609eb17d96ac7e0044da493245d8a2f9762b
xuchen81 <xuchen81@gmail.com>
2024-11-06 09:27:59 +0800

    init

--- test.py
+++ test.py
@@ -1,9 +1,10 @@
 import os
 
-from utils import read_file, get_personalize_text_template
+from utils import read_file, write_to_file, get_personalize_text_template
 from openai import OpenAI
 import json
 from dotenv import load_dotenv
+from html_minifier import clean_html
 
 
 load_dotenv()
@@ -22,8 +23,6 @@ def summarize_company_info():
         {"role": "user", "content": company_info}
     ]
 
-    print(messages)
-
     response = client.chat.completions.create(
         model="gpt-4o-mini",
         messages=messages,
@@ -33,12 +32,11 @@ def summarize_company_info():
     # print(json.dumps(response.to_dict(), indent=2))
     # print(response.choices[0].message.content)
     response_dict = json.loads(response.choices[0].message.content)
-    print(response_dict)
     return response_dict
 
 
 def personalize_text(user_input):
-    system_prompt = read_file('./prompts/personalize_text.txt')
+    system_prompt = read_file('./prompts/personalize_text_v2.txt')
     messages = [
         {"role": "system", "content": system_prompt},
         {"role": "user", "content": user_input}
@@ -53,8 +51,20 @@ def personalize_text(user_input):
     # print(json.dumps(response.to_dict(), indent=2))
     # print(response.choices[0].message.content)
     response_dict = json.loads(response.choices[0].message.content)
+    print("chat messages:")
+    print(messages)
+    print("\n")
+    print("Results:")
+    print("=======" * 20)
     print(response_dict)
+    print("=======" * 20)
     return response_dict
+
+
+def scrape_website():
+    content = read_file("./scraper/page.html")
+    cleaned, classNames = clean_html(content)
+    write_to_file('./scraper/clean_page.html', cleaned)
 
 
 def test():
@@ -90,6 +100,34 @@ def test():
     # print(target_info)
 
 
+def test_v2():
+    to_personalize = {
+        "company_slogo": "Preparing for a recession toolkit",
+        "company_short_description": "A recession readiness toolkit to guide CFOs and finance leaders through recessions and periods of economic instability.",
+        "book_slogo": "2022 CFO Recession Toolkit",
+        "book_overview": "Managers are dealing with a number of economic issues, and some analysts believe that the US is headed toward a recession. To respond to the challenge, take action to address a possible recession now, so that you're ready to minimize the impact and outperform competitors who are not proactive."
+    }
+    company_info = summarize_company_info()
+    personlize_prompt = read_file("./prompts/personalize_text_v2.txt")
+
+    target_info_text = read_file("./tests/target.json")
+
+    # target info object
+    target_info = json.loads(target_info_text)
+
+    for key in target_info.keys():
+        if key in ["Industries", "Personas"]:
+            for subkey in target_info[key].keys():
+                target_audience_info = {
+                    "audience_name": subkey,
+                    "audience_info": target_info[key][subkey]
+                }
+                user_input = get_personalize_text_template(
+                    read_file("./prompts/input.txt"), json.dumps(company_info), json.dumps(target_audience_info), json.dumps(to_personalize))
+                personalize_text(user_input)
+
+
 # testAi()
 # summarize_company_info()
-test()
+test_v2()
+# scrape_website()
--- utils.py
+++ utils.py
@@ -4,6 +4,24 @@ def read_file(file_path):
     return content
 
 
+def write_to_file(file_path, content):
+    with open(file_path, 'w') as file:
+        file.write(content)
+
+
+def int_to_base62(num):
+    base62_chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+    if num == 0:
+        return base62_chars[0]
+
+    base62 = []
+    while num > 0:
+        num, rem = divmod(num, 62)
+        base62.append(base62_chars[rem])
+
+    return ''.join(reversed(base62))
+
+
 def get_personalize_text_template(template_text, company_info, target_info, text):
     personalized_text = template_text.replace("{{company_info}}", company_info)
     personalized_text = personalized_text.replace(
